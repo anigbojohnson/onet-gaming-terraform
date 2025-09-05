f59277823f18956463af7e08e0095b274a143183
Anigbo Johnson <64017426+anigbojohnson@users.noreply.github.com>
2025-09-05 12:03:41 +0000

    check if resources are all destroyed

--- terraform/modules/key/main.tf
+++ terraform/modules/key/main.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "client_key" {
     key_name = "todo-app-key"
-    public_key = file("../modules/key/todo-app-key.pub")
+    public_key = file("terraform/modules/key/todo-app-key.pub")
 }
