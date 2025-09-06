981467d7e34d0cb7e57d63e3b60ab69c31f0b424
Anigbo Johnson <64017426+anigbojohnson@users.noreply.github.com>
2025-09-06 11:57:36 +0000

    change dnf to yum package manager

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -6,6 +6,19 @@ resource "aws_instance" "this" {
   key_name                    = var.key_name
   iam_instance_profile        = var.iam_instance_profile
 
+   user_data = <<-EOF
+              #!/bin/bash
+              # Update packages
+              yum update -y
+
+              # Install Python 3.8
+              amazon-linux-extras enable python3.8
+              yum install -y python3.8
+
+              # Optional: set python3.8 as default
+              alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+              EOF
+
 
   tags = {
     Name = "${var.project_name}-ec2"
