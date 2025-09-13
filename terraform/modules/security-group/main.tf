9eb0560db57c07f85bcc0597121a78e321885e15
Anigbo Johnson <64017426+anigbojohnson@users.noreply.github.com>
2025-09-13 22:57:53 +0000

    add security group for app tier for bastion host

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Replace with your IP
+    cidr_blocks = ["0.0.0.0/0"] 
   }
   
 
