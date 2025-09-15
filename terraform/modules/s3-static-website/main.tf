0c4a5e252908c6cbc47ef0846635f2542a7f99d6
Anigbo Johnson <64017426+anigbojohnson@users.noreply.github.com>
2025-09-15 00:49:24 +0000

    create backend directory

--- terraform/modules/s3-static-website/main.tf
+++ terraform/modules/s3-static-website/main.tf
@@ -13,20 +13,4 @@ resource "aws_s3_bucket_website_configuration" "website" {
 
 }
 
-# Public bucket policy to allow website access
-resource "aws_s3_bucket_policy" "website_policy" {
-  bucket = data.aws_s3_bucket.existing.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "arn:aws:s3:::${data.aws_s3_bucket.existing.id}/${var.website_content_dir}/*"
-      }
-    ]
-  })
-}
