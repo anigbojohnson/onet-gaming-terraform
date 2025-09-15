44cc769a2d369bf20df7d5c6b2bb0437b1b09669
Anigbo Johnson <64017426+anigbojohnson@users.noreply.github.com>
2025-09-15 00:01:14 +0000

    website modules setup

--- terraform/modules/s3-static-website/output.tf
+++ terraform/modules/s3-static-website/output.tf
@@ -1,9 +1,3 @@
-
-# Get website configuration
-data "aws_s3_bucket_website_configuration" "existing" {
-  bucket = data.aws_s3_bucket.existing.id
-}
-
 # Outputs
 output "s3_website_endpoint" {
   value = data.aws_s3_bucket_website_configuration.existing.website_endpoint
