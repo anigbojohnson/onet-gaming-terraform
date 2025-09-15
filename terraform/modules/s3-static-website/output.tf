0f7ac940870b662819ba28780e238534b276ad59
Anigbo Johnson <64017426+anigbojohnson@users.noreply.github.com>
2025-09-15 01:34:24 +0000

    create backend directory

--- terraform/modules/s3-static-website/output.tf
+++ terraform/modules/s3-static-website/output.tf
@@ -1,8 +1,4 @@
 # Outputs
-
-data "aws_s3_bucket" "existing" {
-  bucket = aws_s3_bucket.existing
-}
 output "s3_website_endpoint" {
   value = data.aws_s3_bucket_website_configuration.website.s3_website_endpoint
 }
