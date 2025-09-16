fa5a23038c8bcb79e6ecf772965af834f8440e3f
Anigbo Johnson <64017426+anigbojohnson@users.noreply.github.com>
2025-09-16 13:15:50 +0000

    create backend directory

--- terraform/modules/s3-static-website/main.tf
+++ terraform/modules/s3-static-website/main.tf
@@ -34,7 +34,7 @@ resource "aws_s3_bucket_policy" "website_policy" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "arn:aws:s3:::${data.aws_s3_bucket.existing.id}/${var.website_content_dir}/*"
+        Resource  = "arn:aws:s3:::${data.aws_s3_bucket.existing.id}/*"
       }
     ]
   })
