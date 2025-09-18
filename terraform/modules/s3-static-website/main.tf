ce6158593fd2d579386c5e27aca174c719cea09e
Anigbo Johnson <64017426+anigbojohnson@users.noreply.github.com>
2025-09-18 10:16:28 +0000

    create backend directory

--- terraform/modules/s3-static-website/main.tf
+++ terraform/modules/s3-static-website/main.tf
@@ -25,6 +25,7 @@ resource "aws_s3_bucket_public_access_block" "website" {
 resource "aws_s3_bucket_policy" "website_policy" {
   bucket = aws_s3_bucket.create-bucket.id
 
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
