f923c79df0dfb2f7efae878ae46c55a0379a77f5
Anigbo Johnson <64017426+anigbojohnson@users.noreply.github.com>
2025-09-18 16:22:48 +0000

    create backend directory

--- terraform/modules/s3-static-website/output.tf
+++ terraform/modules/s3-static-website/output.tf
@@ -1,6 +1,6 @@
 # Outputs
 output "s3_website_endpoint" {
-  value = aws_s3_bucket_website_configuration.website.website_endpoint
+  value = aws_s3_bucket_website_configuration.website.website_domain
 }
 
 output "bucket_arn" {
